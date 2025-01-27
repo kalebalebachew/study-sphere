7b2412f50760cbaa68fb51636b352c222b0827a0
Kaleb <Kalebalebachew4@gmail.com>
2025-01-28 01:21:11 +0300

    chore: basic setup

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,21 +1,19 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: { port: 5000 },
+    }
   );
+
+  await app.listen();
+  Logger.log('Auth service is listening on TCP port 5000');
 }
 
 bootstrap();
